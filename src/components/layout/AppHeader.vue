c812e10040fc2a54260a02e60b89cb93dfd5c826
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-07-14 09:04:54 -0300

    FEAT #6: Criação do arquivo AppHeader.vue  início da compojnentização

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/avaliacao">Avaliação</RouterLink>
+        <RouterLink to="/ranking">Ranking</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+</style>
