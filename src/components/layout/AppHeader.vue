2cb41fca4379f90a89ab5a8467fd7b4b11d30844
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-07-14 09:22:00 -0300

    FEAT #6: Começo da componentização do AppHeader.vue e aumentoi na fonte dos links através do main.css

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -4,9 +4,8 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <header>
-
+    <h1>COFFEE QUALITY <br>CHALLENGE</h1>
     <div class="wrapper">
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/avaliacao">Avaliação</RouterLink>
@@ -17,4 +16,14 @@ import { RouterLink } from 'vue-router'
 </template>
 
 <style scoped>
+header {
+  background-color: white;
+  padding: 2vw;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border: solid 2px #9e9e9e;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
