7a7b41d14fd1301df745394d42211bcbba601b36
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-07-17 08:41:28 -0300

    FEAT #6: Continuação do AppHeader, com estilização dos RouterLink e ícone de usuário

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -5,15 +5,21 @@ import { RouterLink } from 'vue-router'
 <template>
   <header>
     <div>
-      <img src="../../../public/Coffe-cafe-logo-by-DEEMKA-STUDIO-4(Creative-Fabrica).jpg" width="200px">
-      <h1>COFFEE QUALITY <br>CHALLENGE</h1>
+      <RouterLink to="/">
+        <img src="/Coffe-cafe-logo-by-DEEMKA-STUDIO-4(Creative-Fabrica).jpg" width="200px">
+      </RouterLink>
+        <h1>COFFEE QUALITY <br>CHALLENGE</h1>
+
     </div>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/avaliacao">Avaliação</RouterLink>
-        <RouterLink to="/ranking">Ranking</RouterLink>
+        <RouterLink to="/" exact-active-class="nav-link-active" active-class="nav-link-active">Home</RouterLink>
+        <RouterLink to="/avaliacao" exact-active-class="nav-link-active" active-class="nav-link-active">Avaliação</RouterLink>
+        <RouterLink to="/ranking" exact-active-class="nav-link-active" active-class="nav-link-active">Ranking</RouterLink>
       </nav>
+    </div>
+    <div>
+      <img src="/user-icon-on-transparent-background-free-png.webp" alt="" width="130px">
     </div>
   </header>
 </template>
@@ -32,5 +38,33 @@ header {
 div {
   display: flex;
   align-items: center;
+}
+nav {
+  display: flex;
+  gap: 1.5rem;
+}
+
+a {
+  margin: 0;
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+  position: relative;
+  padding-bottom: 4px;
+}
+
+a.nav-link-active {
+  color: #8b4513;
+}
+
+a.nav-link-active::after {
+  content: '';
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px;
+  background-color: #8b4513;
+  border-radius: 2px;
 }
 </style>
