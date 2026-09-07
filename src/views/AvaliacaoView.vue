7241ae6e3363d6a872c75eb7eb943f98e34df80e
˜Lucas-ElielL˜ <lucaseliel15032010@gmail.com>
2026-09-06 22:42:16 -0300

    FEAT #12: Começo da página avaliações sem funcionalidades

--- src/views/AvaliacaoView.vue
+++ src/views/AvaliacaoView.vue
@@ -1,7 +1,53 @@
 <script setup>
 </script>
 <template>
-  <h1>Avaliação</h1>
+  <main>
+    <section class="avaliar">
+    <h3>Nova Avaliação</h3>
+    <form action="avaliar">
+      <label for="nome">
+        Nome do Café
+      </label>
+      <input type="text" id="nome">
+      <label for="produtor">
+        Produtor
+      </label>
+      <input type="text" id="produtor">
+      <label for="aroma">
+        Aroma
+      </label>
+        <input type="number" id="aroma">
+      <label for="sabor">
+        Sabor
+      </label>
+      <input type="number" id="sabor">
+      <label for="acidez">
+        Acidez
+      </label>
+      <input type="number" id="acidez">
+      <label for="corpo">
+        Corpo
+      </label>
+      <input type="number" id="corpo">
+      <label for="finalizacao">
+        Finalização
+      </label>
+      <input type="number" id="finalizacao">
+      <p>Média Calculada: 0.0</p>
+      <button type="submit">Salvar Avaliação</button>
+    </form>
+  </section>
+  </main>
 </template>
 <style scoped>
+main {
+  border: solid 3px #bbbbbb;
+  border-top: 0px;
+  background-color: white;
+  padding: 1vw 2vw;
+}
+main form {
+  display: flex;
+  flex-direction: column;
+}
 </style>
